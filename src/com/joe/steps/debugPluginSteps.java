c03e5aba92b96451e5aa7822aacecfb6b333037a
Javier Vildozo <javier.vildozo@bolivia.assuresoft.com>
2017-03-22 16:44:05 -0400

    i found this example and it helps as an introductory to the idea of how to organize your packages and files.

---
+++ src/com/joe/steps/debugPluginSteps.java
@@ -0,0 +1,11 @@
+package com.joe.steps;
+
+import org.jbehave.core.annotations.Given;
+
+public class debugPluginSteps {
+
+	@Given("step represents a precondition to an event")
+	public void debugStepGivenTest(){
+
+	}
+}
